e11eed2f46c11753e05a3614ac4614c54ba47060
Eric Mill <kprojection@gmail.com>
2012-03-13 16:30:52 -0400

    Duh, run the full rake command

--- config/cron/staging/check.sh
+++ config/cron/staging/check.sh
@@ -3,4 +3,4 @@
 . /projects/alarms/.bashrc
 cd /projects/alarms/current
 
-rake $1 > /projects/alarms/shared/cron/check/$1.last 2>&1
+rake subscriptions:check:$1 > /projects/alarms/shared/cron/check/$1.last 2>&1
