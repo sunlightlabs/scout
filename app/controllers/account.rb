e1b27819c81b339a9aa63d2079c09e687edbb8b7
Eric Mill <konklone@gmail.com>
2012-06-01 13:57:20 -0400

    Broke out account controller into a couple other things

--- app/controllers/account.rb
+++ app/controllers/account.rb
@@ -1,104 +1,16 @@
+get '/account/subscriptions' do
+  requires_login
 
-get '/logout' do
-  log_out if logged_in?
-  redirect_back_or '/'
+  erb :"account/subscriptions", :locals => {
+    :interests => current_user.interests.desc(:created_at),
+    :tags => current_user.tags
+  }
 end
 
-get '/login' do
-  @new_user = User.new
-  erb :"account/login"
-end
+get '/account/settings' do
+  requires_login
 
-post '/login' do
-  unless params[:email] and user = User.where(:email => params[:email]).first
-    flash[:user] = "No account found by that email."
-    redirect '/login'
-  end
-
-  if User.authenticate(user, params[:password])
-    log_in user
-    redirect_back_or '/'
-  else
-    flash.now[:user] = "Invalid password."
-    @new_user = User.new
-    erb :"account/login"
-  end
-
-end
-
-post '/account/new' do
-  @new_user = User.new params[:user]
-
-  unless @new_user.password.present? and @new_user.password_confirmation.present?
-    flash[:password] = "Can't use a blank password."
-    redirect "/login"
-  end
-
-  if @new_user.save
-    Admin.new_user @new_user
-    log_in @new_user
-
-    flash[:success] = "Your account has been created."
-    redirect_back_or '/account/settings'
-  else
-    erb :"account/login"
-  end
-end
-
-get '/account/password/forgot' do
-  erb :"account/forgot"
-end
-
-post '/account/password/forgot' do
-  unless params[:email] and user = User.where(:email => params[:email].strip).first
-    flash[:forgot] = "No account found by that email."
-    redirect "/login" and return
-  end
-
-  # issue a new reset token
-  user.new_reset_token
-
-  # email the user with a link including the token
-  subject = "Request to reset your password"
-  body = erb :"account/mail/reset_password", :layout => false, :locals => {:user => user}
-
-  unless user.save and Email.deliver!("Password Reset Request", user.email, subject, body)
-    flash[:forgot] = "Your account was found, but there was an error actually sending the reset password email. Try again later, or write us and we can try to figure out what happened."
-    redirect "/login" and return
-  end
-
-  flash[:forgot] = "We've sent an email to reset your password."
-  redirect "/login"
-end
-
-
-## Account management
-
-get '/account/password/reset' do
-  unless params[:reset_token] and user = User.where(:reset_token => params[:reset_token]).first
-    halt 404 and return
-  end
-
-  # reset the password itself, and the token
-  new_password = user.reset_password
-  user.new_reset_token
-  unless user.save
-    flash[:forgot] = "There was an error issuing you a new password. Please contact us for support."
-    redirect "/login" and return
-  end
-
-  # send the next email with the new password
-
-  subject = "Your password has been reset"
-  body = erb :"account/mail/new_password", :layout => false, :locals => {:new_password => new_password}
-
-  unless Email.deliver!("Password Reset", user.email, subject, body)
-    flash[:forgot] = "There was an error emailing you a new password. Please contact us for support."
-    redirect "/login" and return
-  end
-
-  flash[:forgot] = "Your password has been reset, and a new one has been emailed to you."
-  redirect "/login"
+  erb :"account/settings", :locals => {:user => current_user}
 end
 
 put '/account/settings' do
@@ -126,72 +38,6 @@ put '/account/settings' do
   else
     erb :"account/settings", :locals => {:user => current_user}
   end
-end
-
-get '/account/subscriptions' do
-  requires_login
-
-  erb :"account/subscriptions", :locals => {
-    :interests => current_user.interests.desc(:created_at),
-    :tags => current_user.tags
-  }
-end
-
-put '/account/tag/:name/description' do
-  requires_login
-
-  name = params[:name].strip.downcase
-  unless tag = current_user.tags.where(:name => Tag.deslugify(name)).first
-    halt 404 and return
-  end
-
-  tag.description = params[:description]
-
-  if tag.save
-    description = partial "account/description", :engine => "erb", :locals => {
-      :user => current_user, :tag => tag
-    }
-
-    json 200, {
-      :description_pane => description
-    }
-  else
-    halt 500
-  end
-end
-
-put '/account/tag/:name/public' do
-  requires_login
-
-  name = params[:name].strip.downcase
-  unless tag = current_user.tags.where(:name => Tag.deslugify(name)).first
-    halt 404 and return
-  end
-
-  tag.public = params[:public]
-  tag.save!
-
-  redirect tag_path(current_user, tag)
-end
-
-delete "/account/tags" do
-  requires_login
-
-  names = params[:names].map {|name| Tag.deslugify name}
-
-  names.each do |name|
-    if tag = current_user.tags.where(:name => name).first
-      tag.destroy
-    end
-  end
-
-  redirect "/account/subscriptions"
-end
-
-get '/account/settings' do
-  requires_login
-
-  erb :"account/settings", :locals => {:user => current_user}
 end
 
 
@@ -241,20 +87,4 @@ post '/account/phone/resend' do
 
   flash[:phone] = "We've sent you another verification code."
   redirect "/account/settings"
-end
-
-# login helpers
-
-helpers do
-  def redirect_back_or(path)
-    redirect(params[:redirect].present? ? params[:redirect] : path)
-  end
-  
-  def log_in(user)
-    session['user_email'] = user.email
-  end
-  
-  def log_out
-    session['user_email'] = nil
-  end
 end
