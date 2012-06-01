38f9e0c124a8ec7a0d46fbf8bfd4018bbfbdb0c2
Eric Mill <konklone@gmail.com>
2012-06-01 00:11:25 -0400

    Remove subscription factory, not used

--- test/factories.rb
+++ test/factories.rb
@@ -12,20 +12,5 @@ FactoryGirl.define do
     user
     sequence(:name) {|n| "name#{n}"}
   end
-
-  factory :subscription do
-    user
-
-    association :interest, factory: :search_interest
-    self.interest_in {self.interest ? self.interest.in : "foia"}
-
-    factory :search_subscription do
-      subscription_type "federal_bills"
-      data {
-        self.interest ? self.interest.data : {:query => self.interest_in}
-      }
-    end
-  end
-
-
+  
 end
