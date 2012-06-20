eb181dbde850d348c94183574d3ed4c0035adda5
Eric Mill <konklone@gmail.com>
2012-06-19 23:03:06 -0400

    Tweaked stored info for unsubscribe event

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,13 +2,15 @@ class Event
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :name
+  field :type
   field :description
   field :data, type: Hash
 
+  index :type
+
   def self.unsubscribe!(interest)
     create!(
-      name: "Remove alert", 
+      type: "unsubscribe-alert", 
       description: "#{interest.user.contact} from #{interest.in}", 
       data: interest.attributes.dup
     )
