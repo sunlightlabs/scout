2c4367229a94d8577703174e900e287cad9a2a48
Eric Mill <konklone@gmail.com>
2012-08-12 21:46:21 -0400

    Removed hyphenation

--- deliveries/email.rb
+++ deliveries/email.rb
@@ -158,7 +158,7 @@ module Deliveries
 
     def self.render_final(content)
       content << "----------------\nManage your subscriptions on the web at http://#{config[:hostname]}."
-      content << "\n\nThese notifications are powered by the non-partisan Sunlight Foundation (sunlightfoundation.com), a non-profit that uses cutting-edge technology and ideas to make government transparent and accountable."
+      content << "\n\nThese notifications are powered by the nonpartisan Sunlight Foundation (sunlightfoundation.com), a nonprofit that uses cutting-edge technology and ideas to make government transparent and accountable."
       content << "\n\nReply to this email to send feedback, bug reports, or effusive praise our way."
       content
     end
