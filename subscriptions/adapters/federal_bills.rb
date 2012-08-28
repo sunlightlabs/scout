3a3cec4267f878c01257903d40f667c05ce1ac9a
Eric Mill <konklone@gmail.com>
2012-08-28 11:50:26 -0400

    Adding proper debugging to federal bills adapter

--- subscriptions/adapters/federal_bills.rb
+++ subscriptions/adapters/federal_bills.rb
@@ -127,7 +127,7 @@ module Subscriptions
       # takes parsed response and returns an array where each item is 
       # a hash containing the id, title, and post date of each item found
       def self.items_for(response, function, options = {})
-        return nil unless response['bills']
+        raise AdapterParseException.new("Response didn't include regulations field: #{response.inspect}") unless response['regulations']
         
         response['bills'].map do |bv|
           item_for bv
