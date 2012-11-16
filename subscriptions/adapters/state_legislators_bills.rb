e51cc45ee30171f6d017500cffe991dc13c35983
Paul Tagliamonte <paultag@sunlightfoundation.com>
2012-11-16 15:48:00 -0500

    Updating some sort of ruby classes to do something useful, I think.
     ![](http://img3.ranker.com/list_img/9660/549660/full/the-best-of-the-and-quot-i-don-t-know-what-i-m-doing-and-quot-meme.jpg?version=1350621677000)

---
+++ subscriptions/adapters/state_legislators_bills.rb
@@ -0,0 +1,47 @@
+# Copyright (c) 2012, Sunlight Labs, under the terms of the Scout project's
+# licensing.
+
+module Subscriptions
+  module Adapters
+
+    class StateLegislatorsBills
+      def self.url_for_detail(legislator_id, options = {})
+        api_key = options[:api_key] || config[:subscriptions][:sunlight_api_key]
+        endpoint = "http://openstates.org/api/v1"
+        url = "#{endpoint}/bills/?sponsor_id=#{URI.encode legislator_id}&apikey=#{api_key}"
+        # Currently, we only really care about bills that this person
+        # sponsored. No need to get too fancy yet
+        url
+      end
+
+      def self.search_name(subscription)
+        "Legislator's Bills"
+      end
+
+      def self.short_name(number, interest)
+        "#{number > 1 ? "bills" : "bill"}"
+      end
+
+      def self.items_for(response, function, options = {})
+        raise AdapterParseException.new("Got string response from Open States:\n\n#{response}") if response.is_a?(String)
+
+        response.map {|bill| item_for bill}
+      end
+
+      def self.item_for(bill)
+        # created_at and updated_at are UTC, take them directly as such
+        ['updated_at', 'created_at'].each do |field|
+          bill[field] = bill[field] ? bill[field].to_time : nil
+        end
+
+        SeenItem.new(
+          :item_id => bill['id'],
+          :date => bill["created_at"],
+          :data => bill
+        )
+      end
+    end
+  end
+end
+
+# vim: tabstop=2 expandtab shiftwidth=2 softtabstop=2
