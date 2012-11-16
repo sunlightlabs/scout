24de90037d31bf03c22b3bf9a7cb185b49829e07
Eric Mill <konklone@gmail.com>
2012-11-16 14:47:15 -0500

    Index mechanism field on Receipt

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -9,6 +9,7 @@ class Receipt
   field :user_id
   field :user_email
   field :user_delivery
+  field :mechanism
 
   field :subject
   field :content
@@ -17,10 +18,13 @@ class Receipt
   index delivered_at: 1
   index user_id: 1
   index user_email: 1
+  index mechanism: 1
   
   validates_presence_of :delivered_at
   validates_presence_of :content
 
   # if the user is still around, no harm if it's not
   belongs_to :user
+
+  scope :for_time, ->(start, ending) {where(delivered_at: {"$gt" => Time.zone.parse(start).midnight, "$lt" => Time.zone.parse(ending).midnight})}
 end
