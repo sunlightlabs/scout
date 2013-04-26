518031fcfbb8adf77319bb6c6329356057e5f9c4
Eric Mill <konklone@gmail.com>
2013-04-26 14:33:53 -0400

    Have generic rake script handle command line arguments

--- config/cron/production/rake.sh
+++ config/cron/production/rake.sh
@@ -3,4 +3,6 @@
 . /projects/scout/.bashrc
 cd /projects/scout/current
 
-rake $1 > /projects/scout/shared/cron/$1.last 2>&1
+FIRST=$1
+shift
+rake $FIRST $@ > /projects/scout/shared/cron/$FIRST.last 2>&1
