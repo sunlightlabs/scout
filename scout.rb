23913b6d3e253fb4a70e7253dc33c365610137db
Eric Mill <konklone@gmail.com>
2013-05-30 13:03:35 -0400

    Typography changes, remove header number

--- scout.rb
+++ scout.rb
@@ -11,7 +11,7 @@ set :public_folder, 'public'
 
 # disable sessions in test environment so it can be manually set
 unless test?
-  use Rack::Session::Cookie, 
+  use Rack::Session::Cookie,
     key: 'rack.session',
     path: '/',
     expire_after: (60 * 60 * 24 * 30), # 30 days
@@ -40,17 +40,6 @@ Dir.glob("./app/controllers/*.rb").each {|filename| load filename}
 
 
 before do
-  # interest count is displayed in layout header for logged in users
-  @interest_count = logged_in? ? current_user.interests.count : 0
-
-  # lightweight server-side campaign conversion tracking 
-  # [:utm_source, :utm_medium, :utm_content, :utm_campaign].each do |campaign|
-  #   if params[campaign].present?
-  #     session['campaign'] ||= {}
-  #     session['campaign'][campaign] = params[campaign]
-  #   end
-  # end
-
   @start_time = Time.now
 end
 
@@ -78,24 +67,24 @@ end
 
 # redirector and tracker
 # * records the click event and data, redirects user to final URL
-# 
+#
 # recognized params:
 #   * from - 'email'
 #   * to - url to redirect to (generated at email render time)
-# 
+#
 # And a 'd' hash that can contain:
 #   * remote - a remote service name, e.g. "open_states"
-#   * url_type - 'item' 
-# 
+#   * url_type - 'item'
+#
 # and if this is to a landing:
 #   * item_type - type of landing page
 #   * item_id - ID of landing item
 #   * because - 'search' if a search result, 'item' if an item subscription
 #   * query - for 'search' - search term
 
-get '/url' do 
+get '/url' do
   halt 500 unless params[:to].present?
-  
+
   if params[:from] == "email"
     Event.email_click! (params[:d] || {}).merge(to: params[:to])
   end
@@ -116,12 +105,12 @@ error do
   message = exception.message
 
   request = {
-    method: env['REQUEST_METHOD'], 
+    method: env['REQUEST_METHOD'],
     url: [Environment.config['hostname'], env['REQUEST_URI']].join,
     params: params.inspect,
     user_agent: env['HTTP_USER_AGENT']
   }
-  
+
   if current_user
     request[:user] = current_user.id.to_s
   end
@@ -154,7 +143,7 @@ helpers do
   def google?
     !!(request.env['HTTP_USER_AGENT'] =~ /googlebot/i)
   end
-  
+
   def current_user
     @current_user ||= (session['user_id'] ? User.find(session['user_id']) : nil)
   end
