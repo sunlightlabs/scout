8efc1e5d2cf8f7ea783a5ec38ff665b955bd449f
Eric Mill <eric@konklone.com>
2013-10-18 17:07:57 -0400

    extend definition to handle longer definitions

--- app/models/definition.rb
+++ app/models/definition.rb
@@ -4,12 +4,15 @@ class Definition
 
   field :term
   field :short_definition
-  field :long_definition
+  field :long_definition_text
+  field :long_definition_html
   field :source
   field :source_url
 
   validates_presence_of :term
   validates_presence_of :short_definition
+  validates_presence_of :long_definition_text
+  validates_presence_of :long_definition_html
 
   index term: 1
 end
